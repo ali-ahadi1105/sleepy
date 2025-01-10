bd9bdbde19914288e0670251405b1d57753b6ede
ali-ahadi1105 <aliahadi.f1105@gmail.com>
2025-01-10 21:50:54 +0330

    create a common library for use in all services

---
+++ libs/common/src/config/config.module.ts
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { ConfigModule as NestConfigModule } from '@nestjs/config';
+import * as joi from 'joi';
+
+@Module({
+    imports: [NestConfigModule.forRoot({
+        validationSchema: joi.object({
+            MONGODB_URI: joi.string().required()
+        })
+    })]
+})
+export class ConfigModule {}
+
+
+
+
